bd65504fe512568a282069e9e514b5ca2e62556d
Andy <musictopia2@hotmail.com>
2021-11-12 11:19:56 -0700

    Add project files.

---
+++ CustomMySQLConnectionClass.cs
@@ -0,0 +1,20 @@
+﻿namespace MySQLHelperLibraries;
+public class CustomMySQLConnectionClass : IMySQLConnector
+{
+    EnumDatabaseCategory IDbConnector.GetCategory(IDbConnection connection)
+    {
+        if (connection is MySqlConnection == false)
+        {
+            throw new CustomBasicException("Should have been a mysql connection.  Rethink");
+        }
+        return EnumDatabaseCategory.MySQL;
+    }
+    IDbConnection IDbConnector.GetConnection(EnumDatabaseCategory category, string connectionString)
+    {
+        if (category != EnumDatabaseCategory.MySQL)
+        {
+            throw new CustomBasicException("Only MySQL is supported for this category.  Rethink");
+        }
+        return new MySqlConnection(connectionString);
+    }
+}
